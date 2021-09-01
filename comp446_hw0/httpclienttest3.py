8c413cf45c441ee1b0004b746df2b2935917ac81
Joslenne <joslennepena@gmail.com>
2021-09-01 16:12:13 -0500

    updating test

--- comp446_hw0/httpclienttest3.py
+++ comp446_hw0/httpclienttest3.py
@@ -1,6 +1,7 @@
 import unittest
 import json
-import string
+from json import loads
+
 
 import httpclient
 import httpclienttest2
@@ -11,19 +12,18 @@ class TestHttpClient(httpclienttest2.TestHttpClient):
         client = httpclient.HttpClient('httpbin.org')
         response = client.doPostWithParams('/post', {'Foo' : 'Bar'})
         data = json.loads(response.body)
-        self.assertEquals(data['form']['Foo'], 'Bar')
+        self.assertEqual(data['form']['Foo'], 'Bar')
 
-    def testGetWithParams(self):
-        client = httpclient.HttpClient('httpbin.org')
-        response = client.doGetWithParams('/get', {'Foo' : 'Bar', 'foo' : 'bar'})
-        data = json.loads(response.body)
-        self.assertEquals(data['args'], {'Foo' : 'Bar', 'foo' : 'bar'})
+    # EXTRA CREDIT - DO NOT UNCOMMENT THIS, IT WILL FAIL
+    # ONLY UMCOMMENT AND WORK UNLESS YOU WANT TO COMPLETE THE TEST SO IT PASSES
     
-    def testGetParamEncoding(self):
-        client = httpclient.HttpClient('httpbin.org')
-        response = client.doGetWithParams('/get', {'$foo' : 'bar &z' })
-        data = json.loads(response.body)
-        self.assertEquals(data['args'], {'$foo' : 'bar &z' })
+    #### The second test below fails, if you can debug it then you will receive extra credit.
+    
+    # def testGetWithParams(self):
+    #     client = httpclient.HttpClient('httpbin.org')
+    #     response = client.doGetWithParams('/get', {'Foo' : 'Bar', 'foo' : 'bar'})
+    #     data = json.loads(response.body)
+    #     self.assertEquals(data['args'], {'Foo' : 'Bar', 'foo' : 'bar'})
     
 if __name__ == '__main__':
     unittest.main()
