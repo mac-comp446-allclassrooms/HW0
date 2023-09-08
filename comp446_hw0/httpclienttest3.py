3e26b562aa0531706703b369460bd85a5e7233df
Joslenne <joslennepena@gmail.com>
2023-09-07 20:16:39 -0500

    Update httpclienttest3.py

--- comp446_hw0/httpclienttest3.py
+++ comp446_hw0/httpclienttest3.py
@@ -16,16 +16,12 @@ class TestHttpClient(httpclienttest2.TestHttpClient):
         data = json.loads(response.body)
         self.assertEqual(data['form']['Foo'], 'Bar')
 
-    # EXTRA CREDIT - DO NOT UNCOMMENT THIS, IT WILL FAIL
-    # ONLY UMCOMMENT AND WORK UNLESS YOU WANT TO COMPLETE THE TEST SO IT PASSES
     
-    #### The second test below fails, if you can debug it then you will receive extra credit.
-    
-    # def testGetWithParams(self):
-    #     client = httpclient.HttpClient('httpbin.org')
-    #     response = client.doGetWithParams('/get', {'Foo' : 'Bar', 'foo' : 'bar'})
-    #     data = json.loads(response.body)
-    #     self.assertEquals(data['args'], {'Foo' : 'Bar', 'foo' : 'bar'})
+    def testGetWithParams(self):
+        client = httpclient.HttpClient('httpbin.org')
+        response = client.doGetWithParams('/get', {'Foo' : 'Bar', 'foo' : 'bar'})
+        data = json.loads(response.body)
+        self.assertEquals(data['args'], {'Foo' : 'Bar', 'foo' : 'bar'})
     
 if __name__ == '__main__':
     unittest.main()
