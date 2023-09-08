40b9f812093dce6e9ada1a85ee12bd9acca1be1c
Joslenne <joslennepena@gmail.com>
2023-09-07 20:12:01 -0500

    Update httpclienttest2.py

--- comp446_hw0/httpclienttest2.py
+++ comp446_hw0/httpclienttest2.py
@@ -2,6 +2,9 @@ import json
 import unittest
 import httpclient
 import httpclienttest1
+import tracemalloc
+
+tracemalloc.start()
 
 class TestHttpClient(httpclienttest1.TestHttpClient):
     
@@ -9,7 +12,7 @@ class TestHttpClient(httpclienttest1.TestHttpClient):
         client = httpclient.HttpClient('httpbin.org')
         response = client.doPost('/post', 'Foo=Bar')
         data = json.loads(response.body)
-        self.assertEquals(data['form']['Foo'], 'Bar')
+        self.assertEqual(data['form']['Foo'], 'Bar')
     
 if __name__ == '__main__':
     unittest.main()
